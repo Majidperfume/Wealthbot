1078fe3d48365776a76cabd46ec34a5b83c93db9
Majidperfume <majidgambling@gmail.com>
2026-07-24 18:50:43 +0330

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -1,20 +1,40 @@
-from telegram.ext import Application
+from telegram.ext import (
+    Application,
+    CommandHandler
+)
 
 from config import BOT_TOKEN
 
+from handlers.start import start_command
+
 
 def create_bot():
-    app = Application.builder().token(BOT_TOKEN).build()
+
+    app = Application.builder().token(
+        BOT_TOKEN
+    ).build()
+
+
+    app.add_handler(
+        CommandHandler(
+            "start",
+            start_command
+        )
+    )
+
 
     return app
 
 
+
 def run_bot():
+
     app = create_bot()
 
     print("WealthBot started...")
 
     app.run_polling()
+
 
 
 if __name__ == "__main__":
