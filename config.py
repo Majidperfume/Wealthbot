7d88a005f6d50d6881a4a73001521cdfd98be536
Majidperfume <majidgambling@gmail.com>
2026-07-24 23:07:27 +0330

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("8893799861:AAGFV4ghnz8Sh--1uOGkKafSFaopGTG2wjg")
 
 OWNER_ID = int(os.getenv("OWNER_ID", "0"))
 
