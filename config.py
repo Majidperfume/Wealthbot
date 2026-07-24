6b155e084b9e5ec61cebebcf81d83bbce6a76be8
Majidperfume <majidgambling@gmail.com>
2026-07-24 23:09:34 +0330

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("8893799861:AAGFV4ghnz8Sh--1uOGkKafSFaopGTG2wjg")
 
-OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+BOT_TOKEN = os.getenv(
+    "BOT_TOKEN"
+)
+
+
+OWNER_ID = int(
+    os.getenv(
+        "OWNER_ID",
+        "0"
+    )
+)
+
 
 DATABASE_PATH = "database/wealthbot.db"
+
 
 TIMEZONE = "Asia/Tehran"
