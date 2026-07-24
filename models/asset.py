e5ada96c42277c6b2f1d2fb1a01433350eb1efa9
Majidperfume <majidgambling@gmail.com>
2026-07-24 22:02:29 +0330

    Update asset.py

--- models/asset.py
+++ models/asset.py
@@ -4,26 +4,41 @@ from database.database import db
 class Asset:
 
     @staticmethod
-    def create(name, asset_type, currency_id):
+    def create(name, asset_type_id, currency_id, note=""):
         db.execute(
             """
             INSERT INTO assets
-            (name, asset_type, currency_id)
-            VALUES (?, ?, ?)
+            (name, asset_type_id, currency_id, note)
+            VALUES (?, ?, ?, ?)
             """,
-            (name, asset_type, currency_id),
+            (name, asset_type_id, currency_id, note),
         )
+
 
     @staticmethod
     def all():
         return db.fetchall(
             """
-            SELECT *
+            SELECT
+                assets.id,
+                assets.name,
+                asset_types.name AS type,
+                currencies.code AS currency,
+                assets.note
             FROM assets
-            WHERE active = 1
-            ORDER BY name
+
+            JOIN asset_types
+            ON assets.asset_type_id = asset_types.id
+
+            JOIN currencies
+            ON assets.currency_id = currencies.id
+
+            WHERE assets.active = 1
+
+            ORDER BY assets.id
             """
         )
+
 
     @staticmethod
     def get(asset_id):
@@ -36,26 +51,39 @@ class Asset:
             (asset_id,),
         )
 
+
     @staticmethod
-    def update(asset_id, name, asset_type, currency_id):
+    def update(asset_id, name, asset_type_id, currency_id, note=""):
         db.execute(
             """
             UPDATE assets
+
             SET
                 name = ?,
-                asset_type = ?,
-                currency_id = ?
+                asset_type_id = ?,
+                currency_id = ?,
+                note = ?
+
             WHERE id = ?
             """,
-            (name, asset_type, currency_id, asset_id),
+            (
+                name,
+                asset_type_id,
+                currency_id,
+                note,
+                asset_id,
+            ),
         )
+
 
     @staticmethod
     def delete(asset_id):
         db.execute(
             """
             UPDATE assets
+
             SET active = 0
+
             WHERE id = ?
             """,
             (asset_id,),
