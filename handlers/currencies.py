3d3565eba2f9090b7947ab74b663caf1807f4c27
Majidperfume <majidgambling@gmail.com>
2026-07-24 19:00:32 +0330

    Update currencies.py

--- handlers/currencies.py
+++ handlers/currencies.py
@@ -3,7 +3,10 @@ from telegram.ext import ContextTypes
 
 from keyboards.currencies_menu import get_currencies_menu
 
-from models.currency import get_currencies
+from models.currency import (
+    get_currencies,
+    create_currency
+)
 
 
 async def currencies_menu(
@@ -15,7 +18,6 @@ async def currencies_menu(
         "💱 مدیریت ارزها",
         reply_markup=get_currencies_menu()
     )
-
 
 
 async def list_currencies(
@@ -43,6 +45,84 @@ async def list_currencies(
         )
 
 
+    await update.message.reply_text(text)
+
+
+
+async def add_currency_start(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+):
+
+    context.user_data["adding_currency"] = {
+        "step": 1
+    }
+
+
     await update.message.reply_text(
-        text
+        "نام ارز را وارد کنید:"
     )
+
+
+
+async def add_currency_receive(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+):
+
+    data = context.user_data.get(
+        "adding_currency"
+    )
+
+
+    if not data:
+        return
+
+
+    text = update.message.text
+
+
+    if data["step"] == 1:
+
+        data["name"] = text
+        data["step"] = 2
+
+        await update.message.reply_text(
+            "کد ارز را وارد کنید.\nمثال: USD"
+        )
+
+        return
+
+
+    if data["step"] == 2:
+
+        data["code"] = text.upper()
+        data["step"] = 3
+
+        await update.message.reply_text(
+            "نماد ارز را وارد کنید.\nمثال: $"
+        )
+
+        return
+
+
+    if data["step"] == 3:
+
+        data["symbol"] = text
+
+
+        create_currency(
+            data["name"],
+            data["code"],
+            data["symbol"]
+        )
+
+
+        context.user_data.pop(
+            "adding_currency"
+        )
+
+
+        await update.message.reply_text(
+            f"✅ ارز {data['name']} اضافه شد."
+        )
