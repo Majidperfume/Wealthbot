6168a9e21d4b90653d1089b9125c8a08f9beefa0
Majidperfume <majidgambling@gmail.com>
2026-07-24 23:50:42 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,21 @@
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 
-BOT_TOKEN = os.getenv(
-    "BOT_TOKEN"
-)
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+
+if not BOT_TOKEN:
+    raise ValueError(
+        "BOT_TOKEN پیدا نشد. فایل .env را بررسی کنید."
+    )
 
 
 OWNER_ID = int(
-    os.getenv(
+    os.environ.get(
         "OWNER_ID",
         "0"
     )
